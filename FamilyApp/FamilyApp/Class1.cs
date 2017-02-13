bb2fd6758345b70d5fa8ef6c9c1ae85e532bd8f2
htcstudent <htcstudent@EH255WD08>
2017-02-13 17:56:18 -0600

    Add project files.

---
+++ FamilyApp/FamilyApp/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FamilyApp
+{
+    class Class1 : ContentPage
+    {
+        public Class1()
+        {
+            Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms!",
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                //FontAttributes = FontAttributes.Italic | FontAttributes.Bold,
+                //FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                BackgroundColor = Color.Aqua,
+                TextColor = Color.Red,
+                FormattedText = new FormattedString
+                {
+                    Spans =
+                    {
+                        new Span
+                        {
+                            Text = "There are 4 people in my family." + Environment.NewLine,
+                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+                        },
+                        new Span
+                        {
+                            Text ="Myself" + Environment.NewLine,
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            FontAttributes = FontAttributes.Bold
+                        },
+                        new Span
+                        {
+                            Text = "My dad" + Environment.NewLine,
+                            FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                            FontAttributes = FontAttributes.Bold
+                        },
+                        new Span
+                        {
+                            Text = "My mom" + Environment.NewLine,
+                            FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                            FontAttributes = FontAttributes.Bold
+                        },
+                        new Span
+                        {
+                            Text = "My twin brother" + Environment.NewLine,
+                            FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                            FontAttributes = FontAttributes.Bold
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
